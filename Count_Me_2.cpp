ee7c3225793d0c1f38b37fe71dc6e276a917441d
Md Fahim <firokarimkaji840@gmail.com>
2026-05-04 20:56:23 +0600

    first commit

---
+++ Count_Me_2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main () {
+    int i ;
+    char S [100001];
+    scanf("%s", S);
+    int count = 0;
+    for (i = 0; S[i] != '\0'; i++)
+    {
+        if ( S[i]!='a' 
+            && S[i]!='e' 
+            && S[i]!='i' 
+            && S[i]!='o' 
+            && S[i]!='u' )
+        {
+            count++;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
